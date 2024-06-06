fee80dcbfa9e606a2e2ec27b8ae49fa9c7e3da32
thawan-xyz <thawan.xyz@gmail.com>
2024-06-05 22:40:24 -0300

    update

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,27 @@
-import kotlin.math.*
-
 fun main() {
     val (cakesNeeded, bakingTime, cakesPerBatch, buildOvenTime) = readln().split(" ").map(String::toInt)
 
     var cakesBaked = 0
-    var primaryOvenTimer = 0
-    var secondaryOvenTimer = 0
+    var iOvenTimer = 0
+    var jOvenTimer = 0
 
     while (cakesBaked < cakesNeeded) {
-        primaryOvenTimer += 1
+        iOvenTimer += 1
 
-        if (primaryOvenTimer % bakingTime == 0) {
+        if (iOvenTimer % bakingTime == 0) {
             cakesBaked += cakesPerBatch
         }
 
-        if (primaryOvenTimer > buildOvenTime) {
-            secondaryOvenTimer += 1
-            if (secondaryOvenTimer % bakingTime == 0) {
+        if (iOvenTimer > buildOvenTime) {
+            jOvenTimer += 1
+            if (jOvenTimer % bakingTime == 0) {
                 cakesBaked += cakesPerBatch
             }
         }
     }
 
-    val timeWithOneOven = ceil(cakesNeeded.toDouble() / cakesPerBatch.toDouble()) * bakingTime
-    val timeWithTwoOvens = primaryOvenTimer
+    val timeWithOneOven = ((cakesNeeded + (cakesPerBatch - 1)) / cakesPerBatch) * bakingTime
+    val timeWithTwoOvens = iOvenTimer
 
     println(if (timeWithOneOven > timeWithTwoOvens) "YES" else "NO")
 }
